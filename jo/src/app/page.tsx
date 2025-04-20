6c23c25a7d8559765baaf2ff6ac5394247e63caf
maraiii <mariaeduantunes200281@gmail.com>
2025-04-20 17:02:43 -0300

    estilização e ideias de animação/organização

--- jo/src/app/page.tsx
+++ jo/src/app/page.tsx
@@ -11,46 +11,10 @@ gsap.registerPlugin(useGSAP);
 type CardProps = {
   title: string;
   copy: string;
-  video?: string;
   index: number;
 };
 
-const Card = ({ title, copy, video, index }: CardProps) => {
-  const videoRef = useRef<HTMLVideoElement | null>(null);
-
-  useEffect(() => {
-    const currentVideo = videoRef.current;
-    if (!currentVideo) return;
-
-    const handlePlay = () => {
-      const allVideos = document.querySelectorAll("video");
-      allVideos.forEach((vid) => {
-        if (vid !== currentVideo) {
-          vid.pause();
-        }
-      });
-    };
-
-    currentVideo.addEventListener("play", handlePlay);
-
-    const scrollTrigger = ScrollTrigger.create({
-      trigger: currentVideo,
-      start: "top bottom",
-      end: "bottom top",
-      onLeave: () => {
-        if (!currentVideo.paused) currentVideo.pause();
-      },
-      onLeaveBack: () => {
-        if (!currentVideo.paused) currentVideo.pause();
-      },
-    });
-
-    return () => {
-      currentVideo.removeEventListener("play", handlePlay);
-      scrollTrigger.kill();
-    };
-  }, []);
-
+const Card = ({ title, copy, index }: CardProps) => {
   return (
     <div className="card" id={`card-${index + 1}`}>
       <div className="card-inner">
@@ -59,14 +23,10 @@ const Card = ({ title, copy, video, index }: CardProps) => {
           <p>{copy}</p>
         </div>
         <div className="card-video">
-          {video ? (
-            <video ref={videoRef} controls loop>
-              <source src={`/video/${video}`} type="video/mp4" />
-              Seu navegador não suporta o elemento de vídeo.
-            </video>
-          ) : (
-            <p>Vídeo em breve...</p>
-          )}
+          <iframe
+            src="https://www.youtube.com/embed/nfJ-gShn35g"
+            allowFullScreen
+          ></iframe>
         </div>
       </div>
     </div>
@@ -77,39 +37,34 @@ export default function Home() {
   const cards = [
     {
       title: "Cantata de natal Colégio Inter Ação",
-      copy: "Uma noite em que os alunos do fundamental I do Colégio Inter Ação se reuniram em um evento na qual eles cantariam músicas que celebram o natal.",
-      video: "card-2.mp4",
+      copy: "Uma noite em que os alunos do fundamental I do Colégio Inter Ação se reuniram em um evento no qual eles cantariam músicas que celebram o natal.",
     },
     {
       title: "Casamento Bruna e Johannes",
       copy: "Vídeo de uma cerimônia de casamento do início ao fim, com making of, entrada dos padrinhos, matrimônio e a festa.",
-      video: "card-2.mp4",
     },
     {
       title: "Festa Kvsh",
       copy: "Vídeo de uma festa com a música comandada pelo DJ Kvsh.",
-      video: "card-2.mp4",
     },
     {
       title: "Mostra Científica Colégio Inter Ação",
-      copy: "Evento do Colégio Inter Ação na qual os alunos do Ensino Médio se juntaram para apresentar seus projetos científicos para as famílias visitantes.",
-      video: "card-2.mp4",
+      copy: "Evento do Colégio Inter Ação no qual os alunos do Ensino Médio se juntaram para apresentar seus projetos científicos para as famílias visitantes.",
     },
     {
       title: "Noite de autógrafos",
-      copy: "Um evento na qual os alunos do fundamental I do Colégio Inter Ação autografaram livros escritos por eles mesmos para seus familiares.",
-      video: "card-2.mp4",
+      copy: "Um evento no qual os alunos do fundamental I do Colégio Inter Ação autografaram livros escritos por eles mesmos para seus familiares.",
     },
     {
       title: "Palestra Bullying e Diversidade",
       copy: "A psicóloga Cândida Senna apresentou para os alunos do Fundamental I do Colégio Inter Ação uma palestra sobre a diversidade nas escolas e as consequências do bullying para os alunos.",
-      video: "card-3.mp4",
     },
   ];
 
   const container = useRef<HTMLDivElement | null>(null);
   const textRef = useRef<HTMLHeadingElement | null>(null);
   const heroRef = useRef<HTMLDivElement | null>(null);
+  const pillNavRef = useRef<HTMLDivElement | null>(null);
 
   useGSAP(
     () => {
@@ -121,6 +76,7 @@ export default function Home() {
 
         if (!isLastCard && cardInner) {
           ScrollTrigger.create({
+            markers: true,
             trigger: card,
             start: "top top+=100",
             endTrigger: ".outro",
@@ -147,20 +103,6 @@ export default function Home() {
   );
 
   useEffect(() => {
-    // Animação para o fundo da página
-    if (heroRef.current) {
-      gsap.fromTo(
-        heroRef.current,
-        { opacity: 0, backgroundColor: "#fff" },
-        {
-          opacity: 1,
-          backgroundColor: "#000",
-          duration: 2,
-          ease: "power3.out",
-        }
-      );
-    }
-
     if (textRef.current) {
       const words = textRef.current.querySelectorAll("span");
       gsap.fromTo(
@@ -177,15 +119,37 @@ export default function Home() {
     }
   }, []);
 
+  useEffect(() => {
+    if (pillNavRef.current) {
+      gsap.fromTo(
+        pillNavRef.current,
+        { y: 70, opacity: 0 },
+        {
+          y: 0,
+          opacity: 1,
+          duration: 3,
+          ease: "power2.out",
+        }
+      );
+    }
+  }, []);
+
   const sentence = [
-    { text: "Focus.", color: "#fffff4" },
-    { text: "Illuminate.", color: "#8cbdf8" },
-    { text: "Transform.", color: "#8d7cee" },
+    { text: "J", color: "#000" },
+    { text: "O", color: "#000" },
+    { text: ".", color: "#e5edf5" },
+    { text: "film", color: "#e5edf5" },
   ];
   return (
     <ReactLenis root>
       <div className="app" ref={container}>
         <section className="hero" ref={heroRef}>
+          <section className="pill-nav" ref={pillNavRef}>
+            <button className="pill-btn">Todos</button>
+            <button className="pill-btn">Eventos</button>
+            <button className="pill-btn">Casamentos</button>
+            <button className="pill-btn">Corporativo</button>
+          </section>
           <h1 ref={textRef}>
             {sentence.map((word, index) => (
               <span
@@ -203,7 +167,7 @@ export default function Home() {
         <section className="intro">
           <h1>
             Criando vídeos de destaque para startups que trazem alegria e deixam
-            impressões duradouras.
+            impressões <span>duradouras</span>.
           </h1>
         </section>
         <section className="cards">
@@ -212,7 +176,9 @@ export default function Home() {
           ))}
         </section>
         <section className="outro">
-          <h1>Vamos contar histórias duradouras juntos.</h1>
+          <h1>
+            Vamos contar histórias <span>duradouras</span> juntos.
+          </h1>
           <a
             href={`https://wa.me/${process.env.REACT_APP_WHATSAPP_NUMBER}`}
             target="_blank"
